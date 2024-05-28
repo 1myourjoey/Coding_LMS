6318a25f0e5ebc1204148dad4a859a2cd517fc79
yunyongsu <mistyoo@naver.com>
2024-05-28 10:39:12 +0900

    이전/다음 ing

--- lms_web_service/src/main/java/com/sky/lms_web_service/service/ContentService.java
+++ lms_web_service/src/main/java/com/sky/lms_web_service/service/ContentService.java
@@ -70,12 +70,9 @@ public class ContentService {
         return contentMapper.selectContentsByLectureName(lecName);
     }
 
-<<<<<<< HEAD
     public Integer getPreviousConNum(int lecNum, int conNum) {
         return contentMapper.getPreviousConNum(lecNum, conNum);
     }
-=======
->>>>>>> afee6b4098378d08bd54c73fc6dd41644214012b
 
     public Integer getNextConNum(int lecNum, int conNum) {
         return contentMapper.getNextConNum(lecNum, conNum);
@@ -117,9 +114,4 @@ public class ContentService {
     public List<Contents_Manage> paging(int start, int listSize) {
         return contentMapper.paging((start - 1) * listSize, listSize);
     }
-
-<<<<<<< HEAD
-=======
-
->>>>>>> afee6b4098378d08bd54c73fc6dd41644214012b
 }
