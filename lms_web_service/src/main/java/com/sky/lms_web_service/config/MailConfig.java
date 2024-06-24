54ca6acf3ab7e9014ef519124a3d7e70378f24bc
jincheolyong <ghcy0627@naver.com>
2024-06-24 12:14:35 +0900

    .

--- lms_web_service/src/main/java/com/sky/lms_web_service/config/MailConfig.java
+++ lms_web_service/src/main/java/com/sky/lms_web_service/config/MailConfig.java
@@ -15,8 +15,8 @@ public class MailConfig {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
         javaMailSender.setHost("smtp.naver.com"); // 메일 도메인 서버 주소
-        javaMailSender.setUsername("ghcy0627"); // 메일 유저 이름
-        javaMailSender.setPassword("!cjfdyd9064"); // 메일 패스워드
+        javaMailSender.setUsername("****"); // 메일 유저 이름
+        javaMailSender.setPassword("****"); // 메일 패스워드
 
         javaMailSender.setPort(465); // 메일 인증서버 포트
 
